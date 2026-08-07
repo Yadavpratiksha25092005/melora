d7be6e9dfb944f7382ae096163072388ef224b2b
Yadavpratiksha25092005 <yadavpratiksha488@gmail.com>
2026-08-07 14:47:51 +0530

    Make MinIO optional

--- backend/cmd/main.go
+++ backend/cmd/main.go
@@ -1,30 +1,29 @@
-package main
+﻿package main
 
 import (
-	"log"
-	"net/http"
+    "log"
+    "net/http"
 
-	"spotify-clone-backend/config"
-	"spotify-clone-backend/database"
-	"spotify-clone-backend/pkg/s3"
-	"spotify-clone-backend/routes"
+    "spotify-clone-backend/config"
+    "spotify-clone-backend/database"
+    "spotify-clone-backend/pkg/s3"
+    "spotify-clone-backend/routes"
 )
 
 func main() {
-	cfg := config.Load()
+    cfg := config.Load()
+    db := database.Connect(cfg.DatabaseURL)
+    defer db.Close()
 
-	db := database.Connect(cfg.DatabaseURL)
-	defer db.Close()
+    s3Client, err := s3.NewClient(cfg.S3Endpoint, cfg.S3AccessKey, cfg.S3SecretKey, cfg.S3Bucket, false)
+    if err != nil {
+        log.Printf("warning: unable to connect to MinIO/S3, file uploads will be disabled: %v", err)
+        s3Client = nil
+    }
 
-	s3Client, err := s3.NewClient(cfg.S3Endpoint, cfg.S3AccessKey, cfg.S3SecretKey, cfg.S3Bucket, false)
-	if err != nil {
-		log.Fatalf("unable to connect to MinIO: %v", err)
-	}
-
-	router := routes.Setup(db, cfg.JWTSecret, s3Client)
-
-	log.Printf("server running on :%s (env=%s)", cfg.Port, cfg.Env)
-	if err := http.ListenAndServe(":"+cfg.Port, router); err != nil {
-		log.Fatalf("server failed: %v", err)
-	}
+    router := routes.Setup(db, cfg.JWTSecret, s3Client)
+    log.Printf("server running on :%s (env=%s)", cfg.Port, cfg.Env)
+    if err := http.ListenAndServe(":"+cfg.Port, router); err != nil {
+        log.Fatalf("server failed: %v", err)
+    }
 }
